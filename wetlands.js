bcd15455cb74c60e82d1d8087fd3c7d97bbcb2f2
david rademacher <david.rademacher@mavs.uta.edu>
2020-12-07 00:04:24 -0600

    title displays random list item on hover

--- wetlands.js
+++ wetlands.js
@@ -4,3 +4,12 @@ function randomIcon() {
 	var i = Math.floor(Math.random() * iconArr.length);
 	document.getElementById("icon").setAttribute("href", "./icons/"+iconArr[i]);
 }
+
+// turn 'element' into a random list item!!! wow, new feature.
+function randomItem(element) {
+ var lis = document.querySelector("#main-list").getElementsByTagName("li");
+ var n = Math.floor(Math.random() * lis.length);
+ element.innerHTML = lis[n].innerHTML;
+}
+
+function resetTitle(e) { e.innerHTML = "Post-Quarantine Bucket List"; }
