87e3ac602a4102e45af66a2d7b10261e3e99a9d5
david rademacher <david.rademacher@mavs.uta.edu>
2020-12-09 15:29:04 -0600

    SEDATE_PICCOLO

--- wetlands.js
+++ wetlands.js
@@ -1,14 +1,39 @@
+// get a random int
+function randInt(min, max) {
+	return min + Math.floor((Math.random() * (max - min)));
+}
+
 // we must add icons to this list!   we have to
 var iconArr = ["big_wood.png", "carl_T.png", "conical_frustum.png", "atop.png", "facebook.png", "fear.png", "random_walk.png", "x.png"];
 function randomIcon() {
-	var i = Math.floor(Math.random() * iconArr.length);
-	document.getElementById("icon").setAttribute("href", "./icons/"+iconArr[i]);
+	document.getElementById("icon").setAttribute("href", "./icons/"+iconArr[randInt(0, iconArr.length)]);
 }
 
 // turn 'element' into a random list item!!! wow, new feature.
 function randomItem(e) {
  var lis = document.querySelector("#main-list").getElementsByTagName("li");
- var n = Math.floor(Math.random() * lis.length);
- e.innerHTML = lis[n].innerHTML;
+ e.innerHTML = lis[randInt(0, lis.length)].innerHTML;
 }
 function resetTitle(e) { e.innerHTML = "Post-Quarantine Bucket List"; }
+
+// toggle the display of quite a large number of quotation marks for the fun double quotes game
+var quoteList = ["'","'","'",",","❟","‘","❜","⹂","‛","❛","'̶̛̗̪̗̼͍͒̉̐̒̄̀̾́̍̉̊̇̿̚’"];
+var maxWaveLen = 30, minWaveLen = 10;
+var maxWaves = 50, minWaves = 5;
+function toggleQuotes() {
+	
+	var lmt = document.getElementById("fun-double-quotes-game");
+
+	if (lmt.classList.contains("filled"))  lmt.innerHTML = "";
+	else {
+		var waveLen = randInt(minWaveLen, maxWaveLen);
+		var waves = randInt(minWaves, maxWaves);
+		var wave = "", amplitude = 3 + Math.random() * 5;
+		var ap = quoteList[randInt(0, quoteList.length)];
+
+		for (var j=0; j < waveLen; j++) wave += "<span style=\'font-size: " + (amplitude * Math.abs(j - waveLen/2)) + "px\'>"+ap+"</span>";
+		for (var i=0; i<waves; i++) lmt.innerHTML += wave;
+	}
+	// lmt.scrollIntoView();
+	lmt.classList.toggle("filled");
+}
