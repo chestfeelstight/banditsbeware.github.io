60a86d0855d53a54434f178adab8b854f03f7dcd
raccoonmother <jrcockerham1@gmail.com>
2021-03-10 01:22:05 -0600

    fuck it up if it's your birthday

--- js/wetlands.js
+++ js/wetlands.js
@@ -66,7 +66,7 @@ const vertBeetleTimeout = 6 * 1000;
 
 // number of beetles
 const numHorzBeetles = 16;
-const numVertBeetles = 12;
+const numVertBeetles = 14;
 
 // chance for Kjakman to become covert spy, using alias
 const KjakmanDisappears = .02;
